02e9264aef60d9df99445502d20abaa5b717597a
geetym <godstimemaklu3@gmail.com>
2024-09-02 14:34:35 +0100

    addition

---
+++ src/component/Mission.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const mission = () => {
+  return (
+    <div className="mission h-[40rem] w-full">
+        <div className='text-white py-44 px-10 '>
+            <h2 className="text-2xl py-5 font-bold uppercase"><span className='text-[#86160b] text-6xl font-serif'>O</span>ur Mission</h2>
+            <h1 className="text-5xl font-bold mt-4">
+              <span className='text-7xl text-blue-500'>W</span>e are committed to <br /> shaping a sustainable <span className='text-[#86160b]'>& </span>  <br /> prosperous future for the <br /> food <span className='text-blue-500'>industry</span><span className='text-[#86160b]'>...</span>
+            </h1>
+        </div>
+    </div>
+  )
+}
+
+export default mission
